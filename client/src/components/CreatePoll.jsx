cd7d99768737db53884380790d61df22fd0f51ac
Manhar-Chaurasia <104268663+Manhar-Chaurasia@users.noreply.github.com>
2025-03-30 01:14:46 +0530

    Update CreatePoll.jsx

--- client/src/components/CreatePoll.jsx
+++ client/src/components/CreatePoll.jsx
@@ -86,7 +86,7 @@ const CreatePoll = ({ data }) => {
     const token = localStorage.getItem("authToken");
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/a/create-poll",
+        "https://poll-vault-mern-backend.onrender.com/create-poll",
         formData,
         // {
         //   headers: { Authorization: `Bearer ${token}` },
